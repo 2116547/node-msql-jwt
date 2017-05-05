2522bea0b17f624b76a89533073c29e9a93168ad
Robin Schellius <r.schellius@avans.nl>
2017-05-05 09:11:45 +0200

    Body-parser toegevoegd

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 //
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser')
 var logger = require('morgan');
 var routes_v1 = require('./api/routes_v1');
 var routes_v2 = require('./api/routes_v2');
@@ -10,11 +11,21 @@ var config = require('./config/config');
 var db = require('./config/db');
 
 var app = express();
-var port = process.env.PORT || config.webPort;
+
+// bodyParser zorgt dat we de body uit een request kunnen gebruiken,
+// hierin zit de inhoud van een POST request.
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+// configureer de app
+app.set('port', (process.env.PORT | config.webPort));
+app.set('env', (process.env.ENV | 'development'))
 
 // Installeer Morgan als logger
 app.use(logger('dev'));
 
+// Installeer de routers
 app.use('/api/v1', routes_v1);
 app.use('/api/v2', routes_v2);
 
@@ -26,8 +37,10 @@ app.use('*', function(req, res){
 	});
 });
 
-app.listen(port, function(){
-	console.log('De server luistert op port ' + port);	
+// Installatie klaar; start de server.
+app.listen(app.get('port'), function(){
+	console.log('De server luistert op port ' + app.get('port'));	
 });
 
+// Voor testen met mocha/chai moeten we de app exporteren.
 module.exports = app;
