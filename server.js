53804823cee70556b4f49a5301aec367863275d4
Robin Schellius <r.schellius@avans.nl>
2017-05-18 17:13:08 +0200

    updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 
 // bodyParser zorgt dat we de body uit een request kunnen gebruiken,
 // hierin zit de inhoud van een POST request.
-app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
@@ -25,21 +25,29 @@ app.set('env', (process.env.ENV | 'development'))
 // Installeer Morgan als logger
 app.use(logger('dev'));
 
-// Installeer de routers
+// Deze route is de 'preprocessor'.
+// Hier gaan we later bv. testen of de gebruiker ingelogd is.
+// next() zorgt ervoor dat we 'doorvallen' naar de volgende URL.
+router.get('*', function(req, res, next) {
+    // console.log('aangeroepen.');
+    next();
+});
+
+// Installeer de routers die we gebruiken.
 app.use('/api/v1', routes_v1);
 app.use('/api/v2', routes_v2);
 
 // Fallback - als geen enkele andere route slaagt wordt deze uitgevoerd. 
-app.use('*', function(req, res){
-	res.status(400);
-	res.json({
-		'error' : 'Deze URL is niet beschikbaar.'
-	});
+app.use('*', function(req, res) {
+    res.status(400);
+    res.json({
+        'error': 'Deze URL is niet beschikbaar.'
+    });
 });
 
 // Installatie klaar; start de server.
-app.listen(app.get('port'), function(){
-	console.log('De server luistert op port ' + app.get('port'));	
+app.listen(app.get('port'), function() {
+    console.log('De server luistert op port ' + app.get('port'));
 });
 
 // Voor testen met mocha/chai moeten we de app exporteren.
